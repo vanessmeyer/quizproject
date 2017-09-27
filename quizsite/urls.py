429e2e3cbd1e61e0e675e6f9512daeab2b928415
Vanessa M <vanessa@loop54.com>
2017-09-27 12:29:01 +0200

    Fixed list from step #10.

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -3,7 +3,7 @@ from quiz import views
 
 # this is a list. This list describes the url patterns/paths. The last part of the path references a python method (e.g. views.startpage references the python method def startpage).
 urlpatterns = [
-	path("", views.startpage)
+	path("", views.startpage),
 	path("quiz/<int:quiz_number>/", views.quiz),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question),
 	path("quiz/<int:question_number>/completed/", views.completed),
