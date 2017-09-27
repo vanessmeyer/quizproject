ee4226d4081ab4c89af3c6fd1470e3a42f1afb59
Vanessa M <vanessa@loop54.com>
2017-09-27 15:39:51 +0200

    fixed url stratpage name

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -3,7 +3,7 @@ from quiz import views
 
 # this is a list. This list describes the url patterns/paths. The last part of the path references a python method (e.g. views.startpage references the python method def startpage).
 urlpatterns = [
-	path("", views.startpage, name="stat_page"),
+	path("", views.startpage, name="start_page"),
 	path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
 	path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
