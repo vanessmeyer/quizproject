9d68c430ca2a1b3423395949941fce2b0cb4b846
Vanessa M <vanessa@loop54.com>
2017-09-27 12:16:17 +0200

    Created 4 news views + updated stylesheet links

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def startpage(request):
+	return render(request, "startpage.html")
+
+def quiz(request, quiz_number):
+	return render(request, "quiz.html")
+
+def question(request, quiz_number, question_number):
+	return render(request, "question.html")
+
+def completed(request, quiz_number):
+	return render(request, "completed.html")
