80be1a5f64665443ca8b1e792a7ccf2a3b0830cb
Vanessa M <vanessa@loop54.com>
2017-09-27 14:57:46 +0200

    Added test data to questions view and placeholder in questions html

--- quiz/views.py
+++ quiz/views.py
@@ -36,7 +36,15 @@ def quiz(request, quiz_number):
 	return render(request, "quiz.html", context)
 
 def question(request, quiz_number, question_number):
-	return render(request, "question.html")
+	context = {
+		"question_number": question_number,
+		"question": "Hur många bultar har ölandsbron?",
+		"answer1": "12",
+		"answer2": "66 400",
+		"answer3": "7 428 954",
+		"quiz_number": quiz_number,
+	}
+	return render(request, "question.html", context)
 
 def completed(request, quiz_number):
 	return render(request, "completed.html")
