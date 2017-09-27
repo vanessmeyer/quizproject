ad42e3fa6411adffd16cddfabbe1ded8d9d6ce73
Vanessa M <vanessa@loop54.com>
2017-09-27 15:18:10 +0200

    Gör så att startsidan använder testdata

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -3,9 +3,9 @@ from quiz import views
 
 # this is a list. This list describes the url patterns/paths. The last part of the path references a python method (e.g. views.startpage references the python method def startpage).
 urlpatterns = [
-	path("", views.startpage),
-	path("quiz/<int:quiz_number>/", views.quiz),
-	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question),
-	path("quiz/<int:quiz_number>/completed/", views.completed),
+	path("", views.startpage, name="stat_page"),
+	path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
+	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
+	path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
 
 ]
