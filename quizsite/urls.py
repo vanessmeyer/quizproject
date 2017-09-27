1ec7f6d08c40b5500a1dc09f7a24961cbdb090c0
Vanessa M <vanessa@loop54.com>
2017-09-27 12:25:50 +0200

    Connecting right urls to the right views.
     Koppla ihop rätt URL:er med rätt vyer: Step #10.

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,21 +1,11 @@
-"""quizsite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/dev/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from quiz import views
 
+# this is a list. This list describes the url patterns/paths. The last part of the path references a python method (e.g. views.startpage references the python method def startpage).
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path("", views.startpage)
+	path("quiz/<int:quiz_number>/", views.quiz),
+	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question),
+	path("quiz/<int:question_number>/completed/", views.completed),
+
 ]
