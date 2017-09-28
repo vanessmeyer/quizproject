0378a14cac119307037558cab448aaf7fa8d8a4f
Vanessa M <vanessa@loop54.com>
2017-09-28 10:52:38 +0200

    Created superuser, added Quiz and Question mdoels to Django Admin
    Nu ska vi slå på admingränssnittet så vi enkelt kan skapa frågor och quizzes:

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from quiz.models import Quiz, Question
 
 # Register your models here.
+
+admin.site.register(Quiz)
+admin.site.register(Question)
--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from quiz import views
+from django.contrib import admin
 
 # this is a list. This list describes the url patterns/paths. The last part of the path references a python method (e.g. views.startpage references the python method def startpage).
 urlpatterns = [
@@ -7,5 +8,5 @@ urlpatterns = [
 	path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
 	path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
-
+	path('admin/', admin.site.urls),
 ]
