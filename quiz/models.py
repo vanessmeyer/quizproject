e799a5ef98aa3eab8bba85ac78117567dc5c31dd
Vanessa M <vanessa@loop54.com>
2017-09-28 10:40:28 +0200

    Created migration file and  har skapat två modeller, "Quiz" och "Question"

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Quiz(models.Model):
+		quiz_number = models.PositiveIntegerField()
+		name = models.CharField(max_length=100)
+		description = models.TextField()
+
+class Question(models.Model):
+		question = models.TextField()
+		answer1 = models.CharField(max_length=100)
+		answer2 = models.CharField(max_length=100)
+		answer3 = models.CharField(max_length=100)
+		correct = models.PositiveIntegerField()
+		# This last line makes its so that if a quiz is deleted so are the questions for that quiz. 
+		quiz = models.ForeignKey(Quiz, related_name="quesitons", on_delete=models.CASCADE)
+
+
